3f33e2a074c6c0fbcf2401e3141f4e55276f272b
avazquezp-TEC <avazquezp@tec.mx>
2026-07-03 11:07:32 -0600

    getdata uploaded

---
+++ 0_getData.py
@@ -0,0 +1,17 @@
+import gdown
+import zipfile
+import os
+
+file_id = "1_lU1tOPnjN4B6phRV5Mx05pJ5KfKDqNE"
+
+# Descargar en el directorio actual
+gdown.download(
+    id=file_id,
+    output="dataset.zip",
+    quiet=False
+)
+
+# Extraer en el directorio actual
+with zipfile.ZipFile("dataset.zip", "r") as zip_ref:
+    zip_ref.extractall(".")
+os.remove("dataset.zip")
